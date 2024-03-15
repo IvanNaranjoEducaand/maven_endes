199cbccd01efee565e743173ad933fed16346a9a
IvanNaranjoEducaand <inarmar@g.educaand.es>
2024-03-15 14:03:37 +0100

    Creacion de la clase Empleados

---
+++ src/ejercicio3/Employees.java
@@ -0,0 +1,13 @@
+package ejercicio3;
+
+public class Employees {
+	String nombre;
+	int años;
+	int añosEmpresa;
+	
+	public Employees(String nombre, int an, int anEmp) {
+		this.nombre = nombre;
+		this.años = an;
+		this.añosEmpresa = anEmp;
+	}
+}
