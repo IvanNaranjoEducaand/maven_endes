b78cd28efe9c601d27c0f574922f5dda48e6aa5c
IvanNaranjoEducaand <inarmar@g.educaand.es>
2024-03-15 14:21:16 +0100

    Añadidos comentarios JavaDoc

--- src/ejercicio3/EmployeeManager.java
+++ src/ejercicio3/EmployeeManager.java
@@ -3,15 +3,29 @@ package ejercicio3;
 import java.util.ArrayList;
 
 public class EmployeeManager {
+	/**
+	 * @param empleados LISTA DE EMPLEADOS DINAMICA
+	 */
 	private ArrayList<Employees> empleados = new ArrayList<Employees>();
-	private int numberOfEmployees=0; 
 	
+	/**
+	 * 
+	 * @param name NOMBRE DEL EMPLEADO
+	 * @param years AÑOS QUE LLEVA EL EMPLEADO EN LA EMPRESA
+	 * @return DEVUELVE QUE EL USUARIO SE HALLA AÑADIDO CORRECTAMENTE
+	 * @version 1.0
+	 * @author IVÁN NARANJO MARÍN
+	 */
 	public void addEmployee(String name,int years){ 
 			Employees emp1 = new Employees(name, years);
-			numberOfEmployees++; 
-			System.out.println(name+"added to the system."); 
+			System.out.println(name+" Added to the system."); 
 	} 
 	
+	/**
+	 * @return LISTA DE LOS EMPLEADOS CON SU NOMBRE Y LOS AÑOS QUE LLEVA EN LA EMPRESA
+	 * @version 1.0
+	 * @author IVÁN NARANJO MARÍN
+	 */
 	public void printEmployees(){ 
 		System.out.println("LISTA DE EMPLEADOS: ");
 		for (Employees e : empleados) {
--- src/ejercicio3/Employees.java
+++ src/ejercicio3/Employees.java
@@ -4,11 +4,20 @@ public class Employees {
 	private String nombre;
 	private int años;
 	
+	/**
+	 * 
+	 * @param nombre NOMBRE DEL EMPLEADO
+	 * @param an AÑOS QUE LLEVA EN LA EMPRESA
+	 */
 	public Employees(String nombre, int an) {
 		this.setNombre(nombre);
 		this.setAños(an);
 	}
 
+	/**
+	 * 
+	 * @return NOMBRE DEL EMPLEADO
+	 */
 	public String getNombre() {
 		return nombre;
 	}
@@ -17,6 +26,10 @@ public class Employees {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * 
+	 * @return AÑOS QUE LLEVA EL EMPLEADO EN LA EMPRESA
+	 */
 	public int getAños() {
 		return años;
 	}
